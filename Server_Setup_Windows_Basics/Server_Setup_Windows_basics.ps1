ec0c88d81a6231ce75c959d8565454377facf9ce
Michael Amieva <lainiswired@protonmail.com>
2025-04-06 18:35:17 -0400

    nvm

--- Server_Setup_Windows_Basics/Server_Setup_Windows_basics.ps1
+++ Server_Setup_Windows_Basics/Server_Setup_Windows_basics.ps1
@@ -30,7 +30,6 @@ function  Initialize-FolderStructure {
         "$BasePath",
         "$BasePath\Logs",
         "$BasePath\Configs",
-        "$BasePath\Configs\Cloudflare",
         "$BasePath\Software",
         "$BasePath\Software\Scripts",
         "$BasePath\Backups",
